e24397b6f60efa5a98a290adbb20ea88ba72be39
Alvin-Karanja <alvin2@proton.me>
2023-08-05 12:24:57 +0300

    refactor code

--- src/main-page/house-filter.js
+++ src/main-page/house-filter.js
@@ -1,7 +1,8 @@
 import { useHistory } from "react-router-dom";
 
-const HouseFilter = ({ allHouses }) = {
+const HouseFilter = ({ allHouses }) => {
     const history = useHistory();
+
     const countries = allHouses
         ? Array.from(new Set(allHouses.map((h) => h.country)))
         : [];
@@ -10,12 +11,12 @@ const HouseFilter = ({ allHouses }) = {
     const onSearchChange = (e) => {
         const country = e.target.value;
         history.push(`/searchresults/${country}`);
-    }
+    };
 
     return (
         <div className="row mt-3">
             <div className="offset-md-2 col-md-4">
-                Look for your house in country:
+                Look for your dream house in country:
             </div>
             <div className="col-md-4 mb-3">
                 <select className="form-select" onChange={onSearchChange}>
@@ -28,5 +29,6 @@ const HouseFilter = ({ allHouses }) = {
             </div>
         </div>
     );
-}
+};
+
 export default HouseFilter;
