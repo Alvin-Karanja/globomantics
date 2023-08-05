28acdaea8687b02eb041d78c600a5ae80b210b37
Alvin-Karanja <alvin2@proton.me>
2023-08-05 12:51:07 +0300

    refactor code

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -5,6 +5,7 @@ import Header from "./header";
 import FeaturedHouse from "./featured-house";
 import SearchResults from "../search-results";
 import HouseFilter from "./house-filter";
+import HouseFromQuery from "../house/HouseFromQuery";
 
 function App() {
     const [allHouses, setAllHouses] = useState([]);
@@ -34,11 +35,13 @@ function App() {
                   <Route path="/searchresults/:country">
                       <SearchResults allHouses={allHouses} />
                   </Route>
+
                   <Route path="/house/:id">
                       <HouseFromQuery allHouses={allHouses} />
                   </Route>
+
                   <Route path="/">
-                      <FeaturedHouse house={featuredHouse}></FeaturedHouse>
+                      <FeaturedHouse house={featuredHouse} />
                   </Route>
               </Router>
           </div>
